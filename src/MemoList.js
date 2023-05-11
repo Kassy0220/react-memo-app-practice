a3477e4b7c0a2a5518fb919164dae04adeca132a
github Kassy <github@shinbokutaro0220@gmail.com>
2023-05-12 08:22:57 +0900

    メモがある場合とない場合で表示を切り替えるように変更

--- src/MemoList.js
+++ src/MemoList.js
@@ -1,14 +1,28 @@
 import "./MemoList.css";
 
 function MemoList({ memos, handleMemoItemClick }) {
-  const listItems = memos.map((memo) => (
-    <li className="memo-item" key={memo.id}>
-      <button onClick={() => handleMemoItemClick(memo)}>
-        {memo.content.split("\n")[0]}
-      </button>
-    </li>
-  ));
-  return <ul>{listItems}</ul>;
+  const memoStatus = memos.length
+    ? `${memos.length}個のメモがあります`
+    : "メモはありません";
+
+  const memoList = memos.length ? (
+    <ul>
+      {memos.map((memo) => (
+        <li className="memo-item" key={memo.id}>
+          <button onClick={() => handleMemoItemClick(memo)}>
+            {memo.content.split("\n")[0]}
+          </button>
+        </li>
+      ))}
+    </ul>
+  ) : null;
+
+  return (
+    <>
+      <p id="memo-status">{memoStatus}</p>
+      {memoList}
+    </>
+  );
 }
 
 export default MemoList;
