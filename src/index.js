87c6cdca5327e0053452f9db81d9278be5bff333
github Kassy <github@shinbokutaro0220@gmail.com>
2023-05-19 14:43:43 +0900

    不自然な変数名を修正

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import MemoApp from "./MemoApp";
 
 const storagedMemos = JSON.parse(localStorage.getItem("allMemos"));
-const sampleMemos = storagedMemos ? storagedMemos : [];
+const allMemos = storagedMemos ? storagedMemos : [];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MemoApp memos={sampleMemos} />
+    <MemoApp memos={allMemos} />
   </React.StrictMode>
 );
